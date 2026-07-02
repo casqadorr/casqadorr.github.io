dc8c376996015bf7e832c992c3127edecb5bb0c8
casqadorr <casqadorr@gmail.com>
2026-07-02 19:32:44 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,27 @@
+const CONFIG = {
+
+    VERSION: "CSQ4S v1.0",
+
+    SITE_NAME: "Casqadorr",
+
+    TIMER: 10,
+
+    STEP1: "step1.html",
+
+    STEP2: "step2.html",
+
+    STEP3: "step3.html",
+
+    DOWNLOAD: "download.html",
+
+    INVALID_MESSAGE: "ملفك غير موجود.",
+
+    WELCOME_MESSAGE: "مرحبًا، انزل للأسفل لإكمال التحميل.",
+
+    NEXT_TEXT: "الانتقال للخطوة التالية",
+
+    DOWNLOAD_TEXT: "تحميل الآن",
+
+    COPYRIGHT: "© Casqadorr"
+
+};
