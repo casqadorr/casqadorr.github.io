f4f21ee10cb926552e9778c7af664ee8feca027a
casqadorr <casqadorr@gmail.com>
2026-07-06 17:27:37 +0100

    Create 9.js

---
+++ ads/9.js
@@ -0,0 +1,12 @@
+window.atOptions = {
+    key: "65dbabb611908f80a96ae9a18e5912d9",
+    format: "iframe",
+    height: 250,
+    width: 300,
+    params: {}
+};
+
+const adScript = document.createElement("script");
+adScript.src = "https://www.highperformanceformat.com/65dbabb611908f80a96ae9a18e5912d9/invoke.js";
+adScript.async = true;
+document.currentScript.parentNode.appendChild(adScript);
