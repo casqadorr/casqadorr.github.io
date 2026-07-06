16e7772bd898bdf594bf95ac1a13cea52ef29a63
casqadorr <casqadorr@gmail.com>
2026-07-06 18:30:20 +0100

    Delete 1.js

--- ads/1.js
+++
@@ -1,9 +0,0 @@
-atOptions = {
-    'key' : '88f1ce91331d314d9ef54baeff209e91',
-    'format' : 'iframe',
-    'height' : 60,
-    'width' : 468,
-    'params' : {}
-};
-
-document.write('<script type="text/javascript" src="https://www.highperformanceformat.com/88f1ce91331d314d9ef54baeff209e91/invoke.js"></script>');
